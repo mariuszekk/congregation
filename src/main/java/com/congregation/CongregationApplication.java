e5f7539c93bf347c8f2adac133a0660cc6003079
Mariusz Niznik <Mariusz.Niznik@s3group.com>
2016-11-17 12:12:00 +0100

    First commit Project Congregation

---
+++ src/main/java/com/congregation/CongregationApplication.java
@@ -0,0 +1,12 @@
+package com.congregation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CongregationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CongregationApplication.class, args);
+	}
+}

d07d79d8d70e8ef7c9256c642145e1d9a8d53895
Mariusz Niznik <Mariusz.Niznik@s3group.com>
2016-11-17 15:52:59 +0100

    change version jackson for deserialization data time

--- src/main/java/com/congregation/controller/SourceController.java
+++ src/main/java/com/congregation/controller/SourceController.java
@@ -2,11 +2,11 @@ package com.congregation.controller;
 
 import static java.nio.file.Files.readAllBytes;
 
+import com.congregation.util.ResourceNotFoundException;
 import com.google.common.collect.Maps;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.Resource;
-import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
